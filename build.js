a48140bbdf31679223234b584bda5b3e10a75aec
Robert Kohr <robkohr+git@gmail.com>
2024-11-22 07:51:09 -0800

    update blog

--- build.js
+++ build.js
@@ -242,8 +242,8 @@ articles.forEach(function (article) {
   }
   const filename = `${toKebab(article.title)}`;
 
-  //description content for meta tag needs to be less than 200 characters and no html and new lines should be removed
-  const description = article.content.replace(/<[^>]+>/g, "").replace(/\n/g, "").substring(0, 200);
+  //description content for meta tag needs to be less than 200 characters and no html and now double quotes and new lines should be removed
+  const description = article.content.replace(/<[^>]+>/g, "").replace(/\n/g, "").substring(0, 200).replace
 
   const articleStartHtml = `
         <html>                
